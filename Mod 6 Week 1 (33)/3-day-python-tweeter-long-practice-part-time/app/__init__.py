aaa906bdd25bb3cdc6db8b5c7dc56c3622e0fa60
Pristine Shin <prisvsyo@gmail.com>
2024-10-01 18:06:15 -0700

    edited gitignore and phase 1 after lecture

--- Mod 6 Week 1 (33)/3-day-python-tweeter-long-practice-part-time/app/__init__.py
+++ Mod 6 Week 1 (33)/3-day-python-tweeter-long-practice-part-time/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from app.config import Config
+
+from app.config import Config #this is like a file path, could also do .config bc they're both on the same file level
 
 app = Flask(__name__)
 app.config.from_object(Config)
