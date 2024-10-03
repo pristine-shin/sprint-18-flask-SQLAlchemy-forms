96e271bc69c39af6ff66f9e80f617aefc5c48e44
Pristine Shin <prisvsyo@gmail.com>
2024-10-02 17:44:06 -0700

    done with phase 2 tweeter project, basic routes with inheritance

--- Mod 6 Week 1 (33)/3-day-python-tweeter-long-practice-part-time/app/__init__.py
+++ Mod 6 Week 1 (33)/3-day-python-tweeter-long-practice-part-time/app/__init__.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from random import choice
+from .tweets import tweets
 
 from app.config import Config #this is like a file path, could also do .config bc they're both on the same file level
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    tweet = choice(tweets)
+    print(tweet)
+    return render_template("index.html", tweet=tweet)
+
+
+@app.route('/feed')
+def feed():
+    return render_template("feed.html", tweets=tweets)
