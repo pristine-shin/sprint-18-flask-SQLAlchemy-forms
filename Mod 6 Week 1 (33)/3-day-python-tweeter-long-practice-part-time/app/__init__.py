4924db6a2c1c19a7d7295995961aba183e509aae
Pristine Shin <prisvsyo@gmail.com>
2024-10-03 18:56:21 -0700

    done with phase 4 tweeter project minus bonus

--- Mod 6 Week 1 (33)/3-day-python-tweeter-long-practice-part-time/app/__init__.py
+++ Mod 6 Week 1 (33)/3-day-python-tweeter-long-practice-part-time/app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from .tweets import tweets
+import random
 from random import choice
 from .routes.tweets_routes import tweets_router
 from app.form.form import TweetForm
+from datetime import date
 
 
 from app.config import Config #this is like a file path, could also do .config bc they're both on the same file level
@@ -17,7 +19,25 @@ def index():
     print(tweet)
     return render_template("index.html", tweet=tweet)
 
-@app.route('/new')
+@app.route('/new', methods=['GET', 'POST'])
 def new():
     form = TweetForm()
+    today = date.today()
+    formatted_date = today.strftime("%m/%d/%y")
+    if form.validate_on_submit():
+        new_tweet = {
+            'id': len(tweets),
+            'author': form.data['author'],
+            'tweet': form.data['tweet'],
+            'date': formatted_date,
+            'likes': random.randint(100000, 999999)
+        }
+
+        tweets.append(new_tweet)
+
+        return redirect('/tweets/feed')
+
+    if form.errors:
+        return form.errors
+
     return render_template("new_tweet.html", form=form)
--- Mod 6 Week 1 (33)/3-day-python-tweeter-long-practice-part-time/app/form/form.py
+++ Mod 6 Week 1 (33)/3-day-python-tweeter-long-practice-part-time/app/form/form.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-
+from wtforms.validators import DataRequired
 
 class TweetForm(FlaskForm):
-    author = StringField("Author")
-    tweet = StringField("Tweet")
-    submit = SubmitField("Create Tweet")
+    author = StringField("Author", validators=[DataRequired()])
+    tweet = StringField("Tweet", validators=[DataRequired()])
+    submit = SubmitField("Create Tweet", validators=[DataRequired()])
