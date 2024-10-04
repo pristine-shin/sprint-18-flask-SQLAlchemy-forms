67ab578d2ce9e0a44fb9d7573247e87813495b5b
Pristine Shin <prisvsyo@gmail.com>
2024-10-04 16:46:18 -0700

    i think im doing the sqlite thing properly but my viewer isnt working

--- Mod 6 Week 1 (33)/4-day-python-calender-this-part-time/calendar_db.py
+++ Mod 6 Week 1 (33)/4-day-python-calender-this-part-time/calendar_db.py
@@ -1,18 +1,43 @@
 import sqlite3
+import datetime
 
 DB_FILE = "dev.db"
 
-with sqlite3.connect(DB_FILE) as conn:
-    curs = conn.cursor()
-    curs.execute(
-        '''
-            CREATE TABLE appointments(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name VARCHAR(200) NOT NULL,
-            start_datetime TIMESTAMP NOT NULL,
-            end_datetime TIMESTAMP NOT NULL,
-            description TEXT NOT NULL,
-            private BOOLEAN NOT NULL
-            );
-        '''
-    )
+# with sqlite3.connect(DB_FILE) as conn:
+#     curs = conn.cursor()
+#     curs.execute(
+#         '''
+#             CREATE TABLE appointments(
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             name VARCHAR(200) NOT NULL,
+#             start_datetime TIMESTAMP NOT NULL,
+#             end_datetime TIMESTAMP NOT NULL,
+#             description TEXT NOT NULL,
+#             private BOOLEAN NOT NULL
+#             );
+#         '''
+#     )
+
+
+# with sqlite3.connect(DB_FILE) as conn:
+#     curs = conn.cursor()
+#     curs.execute(
+#         '''
+#         INSERT INTO appointments(name, start_datetime, end_datetime, description, private)
+#         VALUES('My appointment', '2024-10-04 14:00:00', '2024-10-04 15:00:00','An appointment for me', false);
+#         '''
+#     )
+
+
+def get_all_appointments():
+    with sqlite3.connect(DB_FILE) as conn:
+        curs = conn.cursor()
+        curs.execute(
+            '''
+            SELECT * FROM appointments;
+            '''
+        )
+        results = curs.fetchall()
+        print(results)
+
+get_all_appointments()
