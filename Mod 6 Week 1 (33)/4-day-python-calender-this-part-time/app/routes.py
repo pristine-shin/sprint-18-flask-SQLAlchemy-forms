cb1b1b49281e42e4e40d71bace611f369210120d
Pristine Shin <prisvsyo@gmail.com>
2024-10-05 14:52:13 -0700

    pausing on post from forms with sql parameters

--- Mod 6 Week 1 (33)/4-day-python-calender-this-part-time/app/routes.py
+++ Mod 6 Week 1 (33)/4-day-python-calender-this-part-time/app/routes.py
@@ -1,27 +1,38 @@
 from flask import Blueprint, render_template
 import os
 import sqlite3
-import datetime
+from datetime import datetime
+from .forms import AppointmentForm
 
 
 DB_FILE = os.environ.get("DB_FILE")
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def main():
-    with sqlite3.connect(DB_FILE) as conn:
-        curs = conn.cursor()
-        curs.execute(
-            '''
-            SELECT id, name, start_datetime, end_datetime
-            FROM appointments
-            ORDER BY start_datetime;
-            '''
-        )
-        rows = curs.fetchall()
-        starttime_str = '2024-10-04 14:00:00'
-        starttime_obj = datetime.datetime.strptime(starttime_str, '%Y-%m-%d %H:%M:%S')
-        endtime_str = '2024-10-04 15:00:00'
-        endtime_obj = datetime.datetime.strptime(endtime_str, '%Y-%m-%d %H:%M:%S')
-    return render_template('main.html', rows=rows, start=starttime_obj, end=endtime_obj)
+    form = AppointmentForm()
+    if form.validate_on_submit():
+        new_appointment = {
+            'name': form.name.data,
+            'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+            'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+            'description': form.description.data,
+            'private': form.private.data
+        }
+
+        with sqlite3.connect(DB_FILE) as conn:
+            curs = conn.cursor()
+            curs.execute(
+                f'''
+                SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime;
+                '''
+            )
+            rows = curs.fetchall()
+            starttime_str = '2024-10-04 14:00:00'
+            starttime_obj = datetime.datetime.strptime(starttime_str, '%Y-%m-%d %H:%M:%S')
+            endtime_str = '2024-10-04 15:00:00'
+            endtime_obj = datetime.datetime.strptime(endtime_str, '%Y-%m-%d %H:%M:%S')
+        return render_template('main.html', rows=rows, form=form, start=starttime_obj, end=endtime_obj)
