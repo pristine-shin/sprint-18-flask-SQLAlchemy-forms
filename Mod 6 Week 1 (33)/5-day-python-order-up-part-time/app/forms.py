97fae3d50399c19490d1b88b331b2415bd352ae6
Pristine Shin <prisvsyo@gmail.com>
2024-10-07 18:18:42 -0700

    completed making the login view

--- Mod 6 Week 1 (33)/5-day-python-order-up-part-time/app/forms.py
+++ Mod 6 Week 1 (33)/5-day-python-order-up-part-time/app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
-class AppointmentForm(FlaskForm):
+class LoginForm(FlaskForm):
     employee_number = StringField("Employee number", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
