3998ec1df88f4a81cb49cad0716574dad88e549d
Pristine Shin <prisvsyo@gmail.com>
2024-10-09 23:09:59 -0700

    pausing with 16 of 21 passing on PA

--- Assessments/aa18-assessment-practice-flask-pt-main/app/__init__.py
+++ Assessments/aa18-assessment-practice-flask-pt-main/app/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
 from .config import Configuration
-from .models import db
+from .models import db, SimplePerson
 from .routes import simple
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(simple.bp)
 db.init_app(app)
-
+Migrate(app, db)
 
 
 @app.route('/')
 def index():
     return render_template("main_page.html")
+
+
+@app.route('/simple-form-data')
+def data():
+    people = SimplePerson.query.all()
+    return render_template('simple_form_data.html', people=people)
--- Assessments/aa18-assessment-practice-flask-pt-main/app/routes/simple.py
+++ Assessments/aa18-assessment-practice-flask-pt-main/app/routes/simple.py
@@ -1,10 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 from ..forms import SimpleForm
+from app.models import SimplePerson, db
 
 
 bp = Blueprint("simple", __name__, url_prefix="/simple-form")
 
-@bp.route('/')
+@bp.route('/', methods=["GET", "POST"])
 def index():
     form = SimpleForm()
+    if form.validate_on_submit():
+        data = form.data
+        new_user = SimplePerson(name=data['name'],
+                                age=data['age'],
+                                bio=data['bio'],)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/')
+    else:
+        print("Bad Data")
     return render_template("simple_form.html", form=form)
+
+
+
+
